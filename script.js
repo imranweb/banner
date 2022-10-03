986dbaa59319add441477aab9231ce2fa5774516
Mohd Imran <imranweb97@gmail.com>
2022-10-03 15:22:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function testCode() {
-console.log(document.getElementById('page').textContent);
+  console.log(document.getElementById('page').textContent);
+     document.getElementById("testbutton").addEventListener("click", function(){
+      console.log("buttton clicked");
+  });
 }
 testCode();
